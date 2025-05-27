018d38b999375854db30e80c1376b9b08fd13969
Younghun Kim <gyh1457@gmail.com>
2025-05-27 21:22:28 +0900

    Complete Add, Subtract

---
+++ GithubTest/MathUtility.cpp
@@ -0,0 +1,11 @@
+#include "MathUtility.h"
+
+int yh::add(int a, int b)
+{
+	return a + b;
+}
+
+int yh::subtract(int a, int b)
+{
+	return a - b;
+}
---
+++ GithubTest/MathUtility.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace yh
+{
+	int add(int a, int b);
+	int subtract(int a, int b);
+}	
